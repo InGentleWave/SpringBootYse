9788e030932d9d9ae9e091f61f136f9bd813e664
InGentleWave <freya6366@gmail.com>
2025-06-25 18:58:48 +0900

    노트북 연결 테스트용 파일

--- src/main/java/kr/or/ddit/SpringBootYseApplication.java
+++ src/main/java/kr/or/ddit/SpringBootYseApplication.java
@@ -9,5 +9,7 @@ public class SpringBootYseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootYseApplication.class, args);
 	}
+	
+	public void amugeona() {}
 
 }
